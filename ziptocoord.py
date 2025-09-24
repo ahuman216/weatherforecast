f435c690839682d98ac0cbe38abdf94e8cb807d5
ahuman216 <snehippatel@gmail.com>
2025-09-23 20:17:12 -0400

    added excpetion handling with washington dc

--- ziptocoord.py
+++ ziptocoord.py
@@ -1,10 +1,13 @@
 import requests
 def getCoord(zipcode):
-    url = f"http://api.openweathermap.org/geo/1.0/zip?zip={zipcode},US&appid=d0c76440181875c27c4053af158e11a6"
-    response = requests.get(url)
-    r = response.json()
-    lat = r["lat"]
-    
-    lon = r["lon"]
-    name = r["name"]
-    return lat, lon, name
+    try:
+        url = f"http://api.openweathermap.org/geo/1.0/zip?zip={zipcode},US&appid=d0c76440181875c27c4053af158e11a6"
+        response = requests.get(url)
+        r = response.json()
+        lat = r["lat"]
+        
+        lon = r["lon"]
+        name = r["name"]
+        return lat, lon, name
+    except:
+        return [38.8951, -77.0364, "Invalid zipcode, showing temperatures for Washington DC"]
